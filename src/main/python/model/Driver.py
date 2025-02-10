c041b271f6e9df81e40840a1bb1d4b93fe5e9170
Jack Regan <jfr29@duke.edu>
2025-02-10 13:49:39 -0500

    Fix: update logic of file writing method

--- src/main/python/model/Driver.py
+++ src/main/python/model/Driver.py
@@ -1,1 +1,13 @@
+from main.python.model import Model, WriteFile
+
+
 class Driver:
+
+    def runModel():
+        outputData = []
+        
+        outputData.append(["Add varibles in output file"])
+        
+        outputData = Model.runModel(outputData)
+        
+        WriteFile(outputData)
--- src/main/python/model/WriteFile.py
+++ src/main/python/model/WriteFile.py
@@ -2,31 +2,31 @@ import pandas as pd
 from os.path import exists
 
 class WriteFile:
-    
-    def __Init__ (self, output_data):
-        try:
-            if isinstance(output_data, pd.DataFrame):
-                self.output_data = output_data
-        except:
-            print("Output data reading error")
-    
+
         """
         Saves output data frame as a csv called "Defection-Simulation-Output.csv"
         The file is saved to the output folder.
         
         Author: Jack Regan
         """
-    def writeFile (self):        
-        try:
-            outFile = "./main/output/Defection-Simulation-Output.csv"
-            if exists(outFile):
-                i = 1
+def writeFile (self, output_data):
+    try:
+        if not  isinstance(output_data, pd.DataFrame):
+            raise ValueError("Output data must be a Pandas DataFrame.")
+    except Exception as e:
+        print("Output data reading error")  
+        return
+                  
+    try:
+        outFile = "./main/output/Defection-Simulation-Output.csv"
+        if exists(outFile):
+            i = 1
+            outFile = "./main/output/Defection-Simulation-Output-" + str(i) + ".csv"
+            while exists(outFile):
+                i += 1
                 outFile = "./main/output/Defection-Simulation-Output-" + str(i) + ".csv"
-                while exists(outFile):
-                    i += 1
-                    outFile = "./main/output/Defection-Simulation-Output-" + str(i) + ".csv"
                     
-            self.output_data.to_csv(outFile, index=False)
+        output_data.to_csv(outFile, index=False)
         
-        except Exception as e:
-            print(f"Error writing DataFrame: {str(e)} to csv.")
+    except Exception as e:
+        print(f"Error writing DataFrame: {str(e)} to csv.")
